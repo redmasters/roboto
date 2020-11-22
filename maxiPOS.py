a71257eec4f4d4c260ab1193ea69b5b6d2117d7e
Erasmo Jr <erasmo.cjunior@gmail.com>
2020-11-22 20:04:55 -0300

    First Commmit

--- maxiPOS.py
+++ maxiPOS.py
@@ -43,11 +43,44 @@ loja.send_keys(Keys.RETURN)
 # TODO Verificar PDVS com status 0 - Fechado
 # TODO Se TRUE tirar screenshots, caso contrário enviar mensagem de alerta
 
+# Salva screenshot do painel
 time.sleep(2)
 mp.save_screenshot("panel.png")
 
 # TODO Clicar em Consistência LOG
+consistencia = mp.find_element_by_xpath(
+    "/html/body/div[12]/div[2]/span[4]/div[8]"
+).click()
+
 # TODO Clicar em LOJA
+consLoja = mp.find_element_by_xpath(
+    "/html/body/div[12]/div[2]/span[4]/span/div[2]"
+).click()
 # TODO Digitar data e Loja
+constData = mp.find_element_by_xpath(
+    "/html/body/div[13]/form/div[2]/div[1]/div[1]/div"
+).click()
+constData = mp.find_element_by_xpath(
+    "/html/body/div[8]/table/tbody/tr[7]/td[1]"
+).click()
+
+constLoja = mp.find_element_by_xpath(
+    "/html/body/div[13]/form/div[2]/div[1]/div[2]/input"
+)
+constLoja.send_keys("201")
+constLoja.send_keys(Keys.RETURN)
+
 # TODO Clicar em Consistir
+time.sleep(2)
+consistir = mp.find_element_by_xpath(
+    "/html/body/div[13]/form/div[1]/div[3]/div[2]/div[1]"
+).click()
+
 # TODO Salvar ScreenShoot
+time.sleep(2)
+mp.save_screenshot("const.png")
+
+# Encerra o programa
+time.sleep(2)
+mp.quit()
+print("Programa Chegou ao fim")
