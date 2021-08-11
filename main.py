f8cc84b62718307727b957cad3f94ad4fc3b4a9d
Erasmo Jr <erasmo.cjunior@gmail.com>
2021-08-11 13:18:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ url = 'http://pdv.mateus/maxipos_backoffice/app'
 navegador = Driver()
 navegador.go_url(url)
 pausa()
-navegador.login(41806, 41806)
+navegador.login(****, ****)
 pausa()
 navegador.painel_terminal()
 navegador.painel_consistencia()
